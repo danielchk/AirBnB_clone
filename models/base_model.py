cdfd82b61d83e6665979996ca0868c49c32afd7a
danielchk <hollowdaniel1@gmail.com>
2020-06-25 16:31:51 +0000

    test archive and some upgrades

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-import models
+from uuid import uuid4                                                   #used to create a unique id every time
 from datetime import date
 from datetime import datetime
 
@@ -13,9 +13,16 @@ class BaseModel:
     def __init__(self, **kwargs):
         """
         Args:
-        *args:
         **kwargs: dictionary
         """
-        self.id = str(uuid4())                 #unique id representation to each BaseModel in a string type
-        self.created_at=
-    
+        dateform = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid4())                                             #unique id representation to each BaseModel in a string type
+        self.created_at = datetime.today()                                 #date when instance is created
+        self.updated_at = datetime.today()                                 #date when instance is created and updated everytime we change an object
+        if len(kwargs) ! = 0:
+            for key, word in kwargs:
+                if key == "created_at" 
+        
+        def save(self):
+        """"Save command that update updated_at"""
+        self.updated_at = datetime.today()

dc71366b4d9a2fea6f4bdfa8f6066e03070c520a
samisosa20 <1449@holbertonschool.com>
2020-06-25 17:33:03 +0000

    add todict and str

---
+++ models/__init__.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+"""create a unique FileStorage instance for your application"""
+from models.base_model import BaseModel
--- models/base_model.py
+++ models/base_model.py
@@ -9,7 +9,9 @@ Base class to models
 """
 
 class BaseModel:
-    """Base model instantiation"""
+    """
+    Base model instantiation
+    """
     
     def __init__(self, *args, **kwargs):
         """
@@ -17,17 +19,36 @@ class BaseModel:
         **kwargs: dictionary
         """
         dateform = "%Y-%m-%dT%H:%M:%S.%f"
-        self.updated_at = datetime.today()                                 #date when instance is created and updated everytime we change an object
         if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":             #gonna create a datetime object
                     date = datetime.strptime(value, dateform)
         else:
             self.id = str(uuid4())                                             #unique id representation to each BaseModel in a string type
-            self.created_at = datetime.today()                                 #date when instance is created
-            self.
+            self.created_at = datetime.today()                              #date when instance is created
 
-        def save(self):
-        """"Save command that update updated_at"""
-        self.updated_at = datetime.today()
+
+    def save(self):
+        """
+        Save command that update updated_at
+        """
+        self.updated_at = datetime.today()                                 #date when instance is created and updated everytime we change an object
         
+    def __str__(self):
+        """returns a string
+        Return:
+            returns a string of class name, id, and dictionary
+        """
+        return "[{}] ({}) {}".format(
+            type(self).__name__, self.id, self.__dict__)
+    
+    def to_dict(self):
+        """creates dictionary of the class  and returns
+        Return:
+            returns a dictionary of all the key values in __dict__
+        """
+        my_dict = dict(self.__dict__)
+        my_dict["__class__"] = str(type(self).__name__)
+        my_dict["created_at"] = self.created_at.isoformat()
+        my_dict["updated_at"] = self.updated_at.isoformat()
+        return my_dict

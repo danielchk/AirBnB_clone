13afdeb9ea36110894c39f5609a05bc8043ff873
danielchk <hollowdaniel1@gmail.com>
2020-06-25 15:24:58 +0000

    upgrade 1 to base_model

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,21 @@
 #!/usr/bin/python3
+import models
+from datetime import date
+from datetime import datetime
 
 """
 Base class to models
 """
 
-def __init__(self, *args, **kwargs)
+class BaseModel:
+    """Base model instantiation"""
+    
+    def __init__(self, **kwargs):
+        """
+        Args:
+        *args:
+        **kwargs: dictionary
+        """
+        self.id = str(uuid4())                 #unique id representation to each BaseModel in a string type
+        self.created_at=
+    

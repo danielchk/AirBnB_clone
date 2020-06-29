6ad15c1351341f12d67c09e1b2fde6bf5c85ffb4
danielchk <hollowdaniel1@gmail.com>
2020-06-29 21:26:23 +0000

    test cases

---
+++ test_base_model.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Test BaseModel
+"""
+
+import unittest
+import models
+
+class TestBaseModel(unittest.TestCase):
+    """Diferent cases to test BaseModel Class"""
+    
+    

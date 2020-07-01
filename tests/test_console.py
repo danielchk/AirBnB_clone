09541329346e8b1eea80e19c4c867f419a6714b8
danielchk <hollowdaniel1@gmail.com>
2020-07-01 16:31:02 +0000

    more tests

--- tests/test_console.py
+++ tests/test_console.py
@@ -4,7 +4,10 @@ Tests fpor console
 """
 import unittest
 import console
+import pep8
 from console import HBNBCommand
+from unittest.mock import patch
+from io import StringIO
 
 class testConsole(unittest.TestCase):
     """Unittests for console """
@@ -12,3 +15,28 @@ class testConsole(unittest.TestCase):
     def test_prompt(self):
         """test if prompt is correct"""
         self.assertEqual("(hbnb) ", HBNBCommand.prompt)
+
+    def test_pep8_console(self):
+        """test if pep8 works"""
+        style = pep8.StyleGuide(quiet=True)
+        p = style.check_files(["console.py"])
+        self.assertEqual(p.total_errors, 0, 'fix Pep8')
+
+    def test_docstrings(self):
+        """test if docstrings exist alredy"""
+        self.assertIsNotNone(console.__doc__)
+        self.assertIsNotNone(HBNBCommand.emptyline.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_quit.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_EOF.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_create.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_show.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_destroy.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_count.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_all.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_update.__doc__)
+        self.assertIsNotNone(HBNBCommand.default.__doc__)
+
+    def test_empty_line(self):
+        with patch("sys.stdout", new=StringIO()) as out:
+            self.assertFalse(HBNBCommand().onecmd(""))
+            self.assertEqual("", out.getvalue().strip())
